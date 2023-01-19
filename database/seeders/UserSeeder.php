76b209e91c45cd448cf7950e8b7a4695ed7f319e
Dante Cuevas <danieldantecuevas@gmail.com>
2023-01-19 15:11:13 -0500

    excel config

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        User::factory(100)->create();
+    }
+}
